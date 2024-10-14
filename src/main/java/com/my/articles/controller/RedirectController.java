834da20045475a343fd2f5cfb8cc722e469d5f3d
yeeeone <dpdnjs9808@gmail.com>
2024-10-14 13:02:56 +0900

    redirect Test 진행

--- src/main/java/com/my/articles/controller/RedirectController.java
+++ src/main/java/com/my/articles/controller/RedirectController.java
@@ -5,7 +5,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
