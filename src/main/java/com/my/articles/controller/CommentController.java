88b37b0c88e681e196245271774133221cf6baee
yeeeone <dpdnjs9808@gmail.com>
2024-10-16 17:59:26 +0900

    comment CUD 처리 완료

--- src/main/java/com/my/articles/controller/CommentController.java
+++ src/main/java/com/my/articles/controller/CommentController.java
@@ -2,58 +2,62 @@ package com.my.articles.controller;
 
 import com.my.articles.dto.ArticleDTO;
 import com.my.articles.dto.CommentDTO;
+import com.my.articles.entity.Comment;
 import com.my.articles.service.ArticleService;
+import com.my.articles.service.CommentService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @Controller
 @Slf4j
-@RequestMapping("articles")
+@RequiredArgsConstructor
+@RequestMapping("/articles")
 public class CommentController {
-    @Autowired
-    ArticleService articleService;
+    private final CommentService commentService;
 
     //댓글 추가
-    @PostMapping("addComment")
-    public String addComment(@ModelAttribute("newComment") CommentDTO comment, @RequestParam("id") Long id) {
-        log.info("### new comment : " + comment + "article id : " + id);
+    @PostMapping("{id}/comments")
+    public String insertComment(@PathVariable("id") Long articleId, CommentDTO dto) {
+        dto.setId(null);  // 새 댓글을 위한 ID를 null로 설정
+        log.info("### new comment : " + dto);
+        commentService.insertComment(articleId, dto);
+        String url = "redirect:/articles/" + articleId;
+        return url;
+    }
 
-        ArticleDTO article = articleService.findOneArticle(id);
-        comment.setId(null);
-        articleService.addComment(comment);
 
-        log.info("### add comment to article : " + article.getComments());
-        articleService.updateArticle(article);
-        String url = "redirect:" + article.getId();
+    //댓글 삭제
+    @GetMapping("comments/{id}")
+    public String deleteComment(@PathVariable("id") Long id) {
+        Long articleId = commentService.deleteComment(id);
+        String url = "redirect:/articles/" + articleId;
         return url;
     }
 
     //댓글 수정
-    @GetMapping("{id}/{commentId}/update")
-    public String viewUpdateComment(@PathVariable("id") Long id, @PathVariable("commentId") Long commentId, Model model) {
-        CommentDTO comment = articleService.findOneComment(commentId);
-        log.info("### get comment : " + comment);
-        model.addAttribute("comment", comment);
-        String url = "redirect:/articles/" + id;
-        return url;
+    @GetMapping("/comments/view/{id}")
+    public String viewUpdateComment(@PathVariable("id") Long commentId, Model model) {
+        Map<String, Object> map = commentService.findByIdComment(commentId);
+        log.info("### find comment : " + map.get("dto"));
+
+        model.addAttribute("dto", map.get("dto"));
+        model.addAttribute("articleId", map.get("articleId"));  // articleId를 별도로 전달
+
+        return "/articles/update-comment";
     }
 
-    @PostMapping("updateComment")
-    public String updateComment() {
-        String url = "redirect:";
-        return url;
-    }
 
-    //댓글 삭제
-    @GetMapping("{id}/{commentId}/delete")
-    public String deleteComment(@PathVariable("id") Long id, @PathVariable("commentId") Long commentId) {
-        log.info("### delete comment id : " + commentId);
-        articleService.deleteComment(id, commentId);
-
-        String url = "redirect:/articles/"+id;
-        return url;
+    @PostMapping("/{article_id}/comments/{comment_id}")
+    public String updateComment(@PathVariable("article_id") Long article_id, @PathVariable("comment_id") Long comment_id, CommentDTO dto) {
+        log.info("### update comment : " + dto);
+        dto.setId(comment_id);
+        commentService.updateComment(dto);
+        return "redirect:/articles/" + article_id;
     }
 }
