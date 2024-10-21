559b8fdb698afeb67bc91092f65135c6ad219962
yeeeone <dpdnjs9808@gmail.com>
2024-10-21 11:53:59 +0900

    comment 메서드 api 작업 완료

--- src/main/java/com/my/articles/api/CommentApi.java
+++
@@ -1,9 +0,0 @@
-package com.my.articles.api;
-
-import org.springframework.web.bind.annotation.RestController;
-
-//RestController = Controller + ResponseBody
-@RestController
-public class CommentApi {
-
-}
